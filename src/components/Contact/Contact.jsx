fcb84792e9540908caf0f3772036321d33ae3efd
Parth M <miglanilovish5224@gmail.com>
2025-04-08 00:36:01 +0530

    First Draft

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+import styles from "./Contact.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Contact = () => {
+  return (
+    <footer id="contact" className={styles.container}>
+      <div className={styles.text}>
+        <h2>Contact</h2>
+        <p>Feel free to reach out!</p>
+      </div>
+      <ul className={styles.links}>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
+          <a href="mailto:miglanilovish5224@gmail.com">miglanilovish5224@gmail.com</a>
+        </li>
+        <li className={styles.link}>
+          <img
+            src={getImageUrl("contact/linkedinIcon.png")}
+            alt="LinkedIn icon"
+          />
+          <a href="https://www.linkedin.com/in/lovishm ">www.linkedin.com/in/lovishm </a>
+        </li>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
+          <a href="https://www.github.com/lovishlm">github.com/lovishlm</a>
+        </li>
+      </ul>
+    </footer>
+  );
+};
